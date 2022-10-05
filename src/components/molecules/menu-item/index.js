62bc9382943ef6dd4d59f1a569f6376b3762916f
Bara Borneo <49855426+barared28@users.noreply.github.com>
2022-10-05 09:07:14 +0700

    add dropdown menu item & func

--- src/components/molecules/menu-item/index.js
+++ src/components/molecules/menu-item/index.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { ReactComponent as MenuIcon } from '../../../assets/menu.svg';
 import './style.css';
 
-function MenuItem() {
+function ContentDropDown({ handleClose }) {
+    const [first, setFirst] = useState(true);
+
+
+    function helper(e){
+        if (!(document.getElementById('dropdown').contains(e.target))){
+            handleClose();
+        }
+    }
+
+    useEffect(() => {
+        // prevent first click
+        if (!first) {
+            window.addEventListener('click', helper);
+        }
+        return () => {
+            window.removeEventListener('click', helper);
+        }
+    }, [first]);
+
+    useEffect(() => {
+        setFirst(false);
+    }, [])
+
     return (
-        <div>
-            <button className="menu-button">
+        <div className="menu-item-dropdown-container" id="dropdown">
+            <button>Move Right</button>
+            <button>Move Left</button>
+            <button>Edit</button>
+            <button>Delete</button>
+        </div>
+    );
+}
+
+function MenuItem() {
+    const [showDrop, setShowDrop] = useState(false);
+    return (
+        <div className="relative">
+            <button
+                className="menu-button"
+                onClick={() => setShowDrop(true)}
+            >
                 <MenuIcon />
             </button>
+            {showDrop && (
+                <ContentDropDown handleClose={() => setShowDrop(false)} show={showDrop} />
+            )}
         </div>
     );
 }
